77b4245b7d716d91e6cf6739c183d67da3dfe10f
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-18 21:21:35 +0100

    Updated the datamapper configuration

--- models/link.rb
+++ models/link.rb
@@ -12,7 +12,7 @@ class Link
 end
 
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 #DataMapper.setup(:default, "postgres://ubuntu:password@localhost/bookmark_manager_test")
 DataMapper.finalize
 DataMapper.auto_upgrade!
