7847dfa38fb3b3fe9057d7719eac6638ca8a0397
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-18 21:25:18 +0100

    Removing redundant code from link.rb

--- models/link.rb
+++ models/link.rb
@@ -13,6 +13,5 @@ end
 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-#DataMapper.setup(:default, "postgres://ubuntu:password@localhost/bookmark_manager_test")
 DataMapper.finalize
 DataMapper.auto_upgrade!
