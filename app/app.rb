4d121b52a37de24042f0a04b06b0b095d4bfa13b
Tomy8s <tomy8s@hotmail.com>
2016-10-19 12:42:12 +0100

    displays tags correctly

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'models/link'
-require_relative 'models/tag'
+require_relative 'models/data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
@@ -12,6 +11,8 @@ class BookmarkManager < Sinatra::Base
 
   get '/links' do
     @links = Link.all
+    @tags = Tag.all
+    @link_tags = LinkTag.all 
     erb :'links/index'
   end
 
@@ -22,6 +23,7 @@ class BookmarkManager < Sinatra::Base
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
     tag  = Tag.first_or_create(name: params[:tags])
+    p tag.id
     link.tags << tag
     link.save
     redirect '/links'
